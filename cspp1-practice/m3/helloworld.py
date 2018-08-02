1f15d0674136b0db2dbf85b7ca7978c8524ffc75
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-02 10:28:49 +0530

    " -> 1 of 1 passed. pylint: 4.00/10 "

--- cspp1-practice/m3/helloworld.py
+++ cspp1-practice/m3/helloworld.py
@@ -1,1 +1,10 @@
-print("hello world")
+# My first program
+class Hello: # class Hello
+    def __init__(self, message):
+        self.message = message
+
+    def printtext(self): # To print to console
+        print("hello world ", self.message)
+
+HW = Hello("Rajesh")
+HW.printtext()

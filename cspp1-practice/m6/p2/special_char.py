172a8405c15a77f2eeb2aa708d67198ae705b680
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-06 15:23:08 +0530

    " -> 7 of 7 passed. pylint: -2.22/10 "

--- cspp1-practice/m6/p2/special_char.py
+++ cspp1-practice/m6/p2/special_char.py
@@ -7,18 +7,12 @@ def main():
     '''
     Read string from the input, store it in variable str_input.
     '''
-str_in = input()
-str_p = ""
-array = ["!","@","#","$","%","^","&","*"]
-l_q = 0
-for i in range(len(str_in)):
-    flag = 0
-    if str_in[i] in array:
-        str_p += str_in[l_q:i] +  " "
-        flag = 1
-    if flag == 1:
-        l_q = i+1
-    if len(str_p) != len(str_in):
-        str_p += str_in[l_q:]
-print(str_p)
+    s = str(input())
+    z = ""
+    for n in s:
+        if n == "!" or n == "@" or n == "#" or n == "$" or n == "%" or n == "^" or n == "&" or n == "*":
+             z+=" "
+        else:
+            z+=n
+    print(z)
 main()

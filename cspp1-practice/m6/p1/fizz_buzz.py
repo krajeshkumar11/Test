d288c358f8b6b1b2848eea28977d166ebee529b9
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-06 15:35:06 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -12,7 +12,7 @@ def main():
     i = 1
     while i <= num:
         if i%3 == 0 and i%5 == 0:
-            print("FizzBuzz")
+            print("Fizz\nBuzz")
         elif i%3 == 0:
             print("Fizz")
         elif i%5 == 0:

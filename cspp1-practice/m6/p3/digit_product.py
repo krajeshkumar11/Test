8fe82d5cb6ead5926c4401faf928b03aabed5df3
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-06 16:18:21 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -1,24 +1,18 @@
-'''
-Given a  number int_input, find the product of all the digits
-example:
-	input: 123
-	output: 6
-'''
-def main():
-    '''
-    Read any number from the input, store it in variable int_input.
-    '''
-    int_input = int(input())
-    int_input1 = int_input
-    if int_input < 0:
-        int_input1 = -int_input1
-    product = 1
-    string_input = str(int_input1)
-    for i in string_input:
-        product = product * int(i)
-    if int_input < 0 and product != 0:
-        print("-" + str(product))
-    else:
-        print(product)
-if __name__ == "__main__":
-    main()
+"""program to find product of digits"""
+N = int(input())
+COUNT = 0
+PROD = 1
+REM = 0
+TEMP = N
+if TEMP == 0:
+    print(0)
+if TEMP < 0:
+    TEMP = -(TEMP)
+while TEMP > 0:
+    REM = TEMP % 10
+    PROD = PROD * REM
+    TEMP = TEMP // 10
+if N < 0:
+    print(-PROD)
+if N > 0:
+    print(PROD)

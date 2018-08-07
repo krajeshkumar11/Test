7deca0c3ccd9ef287288642b2762cc0c1497dcb2
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-07 13:53:38 +0530

    " -> 0 of 10 passed. pylint: 2.40/10 "

--- cspp1-practice/m7/Functions - Assignment-2/assignment2.py
+++ cspp1-practice/m7/Functions - Assignment-2/assignment2.py
@@ -55,8 +55,8 @@ def payingDebtOffInAYear(balance, annualInterestRate):
 
 
 def main():
-    # data = input()
-    data = "4773 0.2"
+    data = input()
+    # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
     print(payingDebtOffInAYear(data[0],data[1]))

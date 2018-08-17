367bfab7575f4ca0b8d6e53e4c5eaccad56c502b
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-17 12:02:09 +0530

    " -> 1 of 3 passed. pylint: 9.84/10 "

--- cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
@@ -13,8 +13,9 @@ def clean_up(data):
     updated_data_list = []
     # print(data_list)
     for each_word in data_list:
-        updated_data_list.append(re.sub("[^a-z]+", "", each_word).strip())
-
+        # each_word_list = each_word.split("'")
+        updated_data_list.append(re.sub("[^a-z']+", "", each_word).strip())
+    # print(updated_data_list)
     return updated_data_list
 
 def remove_stop_words(words_list):
@@ -22,20 +23,24 @@ def remove_stop_words(words_list):
         Compute to remove stop words from given word list.
     """
     stop_words = load_stopwords("stopwords.txt")
-    twmp_words_list = words_list[:]
-    for each_word in twmp_words_list:
+    temp_words_list = sorted(words_list[:])
+    # print(temp_words_list)
+    for each_word in temp_words_list:
         if each_word in stop_words:
+            # print(each_word, "DELETED")
             words_list.remove(each_word)
-
+    # print(words_list)
     return words_list
 
 def get_words_frequency(words_list1, words_list2):
     """
         Compute word frequency from given word lists.
     """
+    # print(len(words_list1), len(words_list2))
     word_frequency = {}
     for each_word in words_list1:
         if each_word not in word_frequency:
+            # print(each_word)
             word_frequency[each_word] = [1, 0]
         else:
             frequency_list = word_frequency[each_word]
@@ -44,12 +49,13 @@ def get_words_frequency(words_list1, words_list2):
 
     for each_word in words_list2:
         if each_word not in word_frequency:
+            # print(each_word)
             word_frequency[each_word] = [0, 1]
         else:
             frequency_list = word_frequency[each_word]
             frequency_list[1] += 1
             word_frequency[each_word] = frequency_list
-
+    # print(len(word_frequency.keys()), "LENGTH")
     return word_frequency
 
 def similarity(dict1, dict2):
@@ -58,13 +64,16 @@ def similarity(dict1, dict2):
     '''
     list_one = clean_up(dict1)
     list_one = remove_stop_words(list_one)
+    # print(list_one)
     list_two = clean_up(dict2)
     list_two = remove_stop_words(list_two)
+    # print(list_two)
 
     word_frequency = get_words_frequency(list_one, list_two)
     numerator_product = []
     for each in word_frequency:
         each_word_freq = word_frequency[each]
+        # print(each_word_freq)
         numerator_product.append(each_word_freq[0] * each_word_freq[1])
 
     # print(numerator_product)
@@ -75,8 +84,9 @@ def similarity(dict1, dict2):
 
     for each in word_frequency:
         each_word_freq = word_frequency[each]
-        denominator_square1.append(each_word_freq[0]^2)
-        denominator_square2.append(each_word_freq[1]^2)
+
+        denominator_square1.append(each_word_freq[0]**2)
+        denominator_square2.append(each_word_freq[1]**2)
 
     denominator = math.sqrt(sum(denominator_square1)) * math.sqrt(sum(denominator_square2))
     # print(math.sqrt(sum(denominator_square1)) * math.sqrt(sum(denominator_square2)))

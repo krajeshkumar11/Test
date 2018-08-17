0fa99a8a6a922acb4dcac2cf3e1c739caad02a4f
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-17 12:50:11 +0530

    " -> 2 of 3 passed. pylint: 9.32/10 "

--- cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
@@ -8,34 +8,33 @@ def clean_up(data):
     """
         Compute to remove non srings from given string and return list.
     """
-    data_list = data.lower().split(" ")
-    updated_data_list = []
-    # print(data_list)
-    for each_word in data_list:
-        # each_word_list = each_word.split("'")
-        updated_data_list.append(re.sub("[^a-z']+", "", each_word.strip()))
-    # print(updated_data_list)
-    return updated_data_list
+    # data_list = data.lower().split(" ")
+    # updated_data_list = []
+    # # print(data_list)
+    # for each_word in data_list:
+    #     # each_word_list = each_word.split("'")
+    #     updated_data_list.append(re.sub("[^a-z']+", "", each_word.strip()))
+    # # print(updated_data_list)
+    # return updated_data_list
+
+    return [re.sub("[^a-z']+", "", each_word.strip()) for each_word in data.lower().split(" ")]
 
 def remove_stop_words(words_list):
     """
         Compute to remove stop words from given word list.
     """
     stop_words = load_stopwords("stopwords.txt")
-    temp_words_list = sorted(words_list[:])
-    # print(temp_words_list)
+    temp_words_list = words_list[:]
     for each_word in temp_words_list:
         if each_word in stop_words:
-            # print(each_word, "DELETED")
             words_list.remove(each_word)
-    # print(words_list)
+
     return words_list
 
 def get_words_frequency(words_list1, words_list2):
     """
         Compute word frequency from given word lists.
     """
-    # print(len(words_list1), len(words_list2))
     word_frequency = {}
     for each_word in words_list1:
         if each_word not in word_frequency:
@@ -48,26 +47,14 @@ def get_words_frequency(words_list1, words_list2):
 
     for each_word in words_list2:
         if each_word not in word_frequency:
-            # print(each_word)
             word_frequency[each_word] = [0, 1]
         else:
             frequency_list = word_frequency[each_word]
             frequency_list[1] += 1
             word_frequency[each_word] = frequency_list
-    # print(len(word_frequency.keys()), "LENGTH")
     return word_frequency
 
-def similarity(dict1, dict2):
-    '''
-        Compute the document distance as given in the PDF
-    '''
-    list_one = clean_up(dict1)
-    list_one = remove_stop_words(list_one)
-    # print(list_one)
-    list_two = clean_up(dict2)
-    list_two = remove_stop_words(list_two)
-    # print(list_two)
-
+def compute_distance(list_one, list_two):
     word_frequency = get_words_frequency(list_one, list_two)
     numerator_product = []
     for each in word_frequency:
@@ -88,9 +75,24 @@ def similarity(dict1, dict2):
         denominator_square2.append(each_word_freq[1]**2)
 
     denominator = int(math.sqrt(sum(denominator_square1)) * math.sqrt(sum(denominator_square2)))
+    # print(numerator)
     # print(denominator)
+    # result_value = numerator/denominator
+    # if int(result_value) - result_value < 0.1:
+    #     return math.ceil(result_value)
 
-    return float(numerator/denominator)
+    return numerator/denominator
+
+def similarity(d1, d2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    list_one = clean_up(d1)
+    list_one = remove_stop_words(list_one)
+    list_two = clean_up(d2)
+    list_two = remove_stop_words(list_two)
+
+    return compute_distance(list_one, list_two)
 
 
 def load_stopwords(filename):

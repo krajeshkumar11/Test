c34e2976c0ad2f900c9b9ae0ff9fec623f93bdae
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-08-23 10:29:46 +0530

    " -> 4 of 8 passed. pylint: 7.80/10 "

---
+++ cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -0,0 +1,88 @@
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    mm1 = len(m1)
+    nm1 = len(m1[0])
+    mm2 = len(m2)
+    nm2 = len(m2[0])
+    result_matrix = [0] * nm1
+    for i in range(nm1):
+        result_matrix[i] = [0] * mm1
+    if nm1 == mm2:
+        for i in range(mm1):
+            for j in range(nm2):
+                for k in range(mm2):
+                    result_matrix[i][j] += m1[i][k] * m2[k][j]
+
+        return result_matrix
+    else:
+        return 'Error: Matrix shapes invalid for addition'
+
+def add_matrix(m1, m2):
+    '''
+        check if the matrix shapes are similar
+        add the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for addition
+        and return None
+        error message should be "Error: Matrix shapes invalid for addition"
+    '''
+    mm1 = len(m1)
+    nm1 = len(m1[0])
+    mm2 = len(m2)
+    nm2 = len(m2[0])
+    result_matrix = [0] * nm1
+    for i in range(nm1):
+        result_matrix[i] = [0] * mm1
+
+    if mm1 == mm2 and nm1 ==nm2:
+        for i in range(mm1):
+            for j in range(nm1):
+                result_matrix[i][j] = m1[i][j] + m2[i][j]
+        return result_matrix
+    else:
+        return 'Error: Matrix shapes invalid for addition'
+
+def read_matrix():
+    '''
+        read the matrix dimensions from input
+        create a list of lists and read the numbers into it
+        in case there are not enough numbers given in the input
+        print an error message and return None
+        error message should be "Error: Invalid input for the matrix"
+    '''
+    new_matrix_size = input().split(',')
+    new_matrix_size = list(map(int, new_matrix_size))
+
+    m = new_matrix_size[0]
+    n = new_matrix_size[1]
+    new_matrix = [0] * n
+
+    for i in range(n):
+        new_matrix[i] = [0] * m
+
+    for i in range(new_matrix_size[0]):
+        row_data = input().split(' ')
+        row_data = list(map(int, row_data))
+        for j in range(new_matrix_size[1]):
+            new_matrix[i][j] = row_data[j]
+    return new_matrix
+
+def main():
+    # read matrix 1
+    matrix_1 = read_matrix()
+    # read matrix 2
+    matrix_2 = read_matrix()
+    # print(matrix_1)
+    # print(matrix_2)
+    # add matrix 1 and matrix 2
+    print(add_matrix(matrix_1, matrix_2))
+    # multiply matrix 1 and matrix 2
+    print(mult_matrix(matrix_1, matrix_2))
+
+if __name__ == '__main__':
+    main()
